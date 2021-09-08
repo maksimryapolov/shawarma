4091e1930ec0a4c2102d9631d3d6e9727fa85560
unknown <maks.riapolov@yandex.ru>
2021-09-09 01:32:06 +0300

    сохранение свойств в модалке

--- src/blocks/components/popup/script.js
+++ src/blocks/components/popup/script.js
@@ -4,9 +4,8 @@
     document.querySelectorAll(".popup").forEach((item) => {
         const idProduct = item.getAttribute("id").substr(0, item.getAttribute("id").indexOf(prefixIdForModal));
         const entity = document.querySelector(`#${idProduct}`);
-        const titleModal = entity.querySelector("[data-title]")?.dataset.title;
-        
-        item.querySelector(".popup__title").textContent = titleModal;
+
+        setContentModal(entity, item);        
 
         document.querySelector(`#${idProduct}`)?.addEventListener("click", function() {
             if(this.getAttribute("id") !== "" && this.getAttribute("id")) {
@@ -32,9 +31,28 @@
     }
     
     function closeCurrentModal(modal) {
+        modal.classList.remove("popup__show");
         modal.querySelector(".popup__bg")?.classList.remove("popup__bg-show");
         modal.querySelector(".popup__body")?.classList.remove("popup__body-show");
-        modal.classList.remove("popup__show");
     }
 
+    document.addEventListener("keyup", function(e) {
+        const modal = this.querySelector(".popup__show");
+        if(e.code === "Escape" && modal) {
+            closeCurrentModal(modal);
+        }
+    });
+
+    function setContentModal(entity, el) {
+        /* Получение и сохр. свойств */
+        const titleModal = entity.querySelector("[data-title]")?.dataset.title;
+        const descModal = entity.querySelector("[data-desc]")?.dataset.desc;
+        const sizeModal = entity.querySelector("[data-size]")?.dataset.size;
+        const priceModal = entity.querySelector("[data-price]")?.dataset.price;
+
+        el.querySelector(".popup__title").innerHTML = titleModal;
+        el.querySelector(".popup__text-item").innerHTML = descModal;
+        el.querySelector(".popup__size").innerHTML = sizeModal;
+        el.querySelector(".popup__price").innerHTML = priceModal;
+    }
 })();
