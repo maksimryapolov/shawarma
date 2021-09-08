0c7a67249dc9797ce44506d8b403cd92786074ea
unknown <maks.riapolov@yandex.ru>
2021-09-09 00:45:43 +0300

    save popup

--- src/blocks/components/popup/script.js
+++ src/blocks/components/popup/script.js
@@ -1,44 +1,39 @@
 (() => {
-    const productItems = document.querySelectorAll(".cards__item");
     const prefixIdForModal = "-modal";
-    // const btnClose = document.querySelectorAll()
-    let popupItems = [];
 
-    productItems.forEach((item) => {
-        let popupItem = document.querySelector("#" + item.getAttribute("id") + prefixIdForModal);
-        // let nameProduct = item.querySelector(".popup__title");
-        // let moreInfoBtn = item.querySelector("");
-        let btnsClose = item.querySelector(".popup__close");
+    document.querySelectorAll(".popup").forEach((item) => {
+        const idProduct = item.getAttribute("id").substr(0, item.getAttribute("id").indexOf(prefixIdForModal));
+        const entity = document.querySelector(`#${idProduct}`);
+        const titleModal = entity.querySelector("[data-title]")?.dataset.title;
+        
+        item.querySelector(".popup__title").textContent = titleModal;
 
-
-        item.addEventListener("click", function() {
+        document.querySelector(`#${idProduct}`)?.addEventListener("click", function() {
             if(this.getAttribute("id") !== "" && this.getAttribute("id")) {
-                showCurrentModal(document.querySelector("#" + item.getAttribute("id") + prefixIdForModal));
+                showCurrentModal(document.querySelector(`#${this.getAttribute("id")}${prefixIdForModal}`));
             }
         });
 
-        btnsClose.addEventListener("click", function () {
+        item.querySelector(".popup__close")?.addEventListener("click", function () {
             closeCurrentModal(this.closest(".popup"));
         });
 
-        console.log(popupItem);
-
-        popupItems.push();
+        item.querySelector(".popup__bg")?.addEventListener("click", function(e) {
+            if(e.target == this) {
+                closeCurrentModal(this.closest(".popup"));
+            }
+        });
     });
 
-    console.log(popupItems);
-    // console.log(productItems);
-
     function showCurrentModal(modal) {
-        // Добавить стили модалки
         modal.classList.add("popup__show");
         modal.querySelector(".popup__bg")?.classList.add("popup__bg-show");
         modal.querySelector(".popup__body")?.classList.add("popup__body-show");
     }
     
     function closeCurrentModal(modal) {
-        modal.querySelector(".popup__body")?.classList.remove("popup__body-show");
         modal.querySelector(".popup__bg")?.classList.remove("popup__bg-show");
+        modal.querySelector(".popup__body")?.classList.remove("popup__body-show");
         modal.classList.remove("popup__show");
     }
 
